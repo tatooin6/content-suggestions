faf003e1bf3bb6c808c16d4573379076ea1c24e5
Antonio Pantoja <tato_fos@outlook.com>
2025-04-27 23:13:33 -0400

    Include link to Tato page on footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -3,7 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; {new Date().getFullYear()} JourBuddy. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()}  {" "}
+      <a href="https://tato-portfolio.vercel.app" target="_blank" rel="noopener noreferrer">
+          Tato
+      </a>
+      . All rights reserved.</p>
     </footer>
   );
 };
